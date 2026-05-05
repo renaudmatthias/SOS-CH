b13a0f7b1fcab352eacacbc10b6876f17ecdbc98
Matthias Renaud <matthiasrenaud238@gmail.com>
2026-05-05 15:13:50 +0200

    test 7

--- script.js
+++ script.js
@@ -1,69 +1,50 @@
-// --- Projection LV95 ---
-const extent = [2420000, 1030000, 2900000, 1360000];
-
+// --- Définition de la projection LV95 ---
 proj4.defs(
   "EPSG:2056",
   "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 " +
   "+x_0=2600000 +y_0=1200000 +ellps=bessel " +
   "+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
 );
-
 ol.proj.proj4.register(proj4);
 
 const projection = new ol.proj.Projection({
   code: "EPSG:2056",
-  extent
+  extent: [2420000, 1030000, 2900000, 1360000]
 });
 
-let pointLayer;
-
-// --- Carte ---
+// --- Carte avec WMTS swisstopo ---
 const map = new ol.Map({
   target: "map",
   layers: [
     new ol.layer.Tile({
-      source: new ol.source.TileWMS({
-        url: "https://wms.geo.admin.ch/",
-        params: {
-          LAYERS: "ch.swisstopo.pixelkarte-farbe",
-          FORMAT: "image/png"
-        },
-        serverType: "mapserver"
+      source: new ol.source.WMTS({
+        url: "https://wmts.geo.admin.ch/1.0.0/{Layer}/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
+        layer: "ch.swisstopo.pixelkarte-farbe",
+        matrixSet: "2056",
+        format: "image/jpeg",
+        style: "default",
+        tileGrid: new ol.tilegrid.WMTS({
+          origin: [2420000, 1350000],
+          resolutions: [
+            4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000,
+            1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5
+          ],
+          matrixIds: [
+            "0","1","2","3","4","5","6","7","8","9","10","11",
+            "12","13","14","15","16","17","18","19","20","21","22"
+          ]
+        })
       })
     })
   ],
   view: new ol.View({
     projection,
-    center: [2550000, 1207000],
-    zoom: 5,
-    extent: [2485000, 1075000, 2834000, 1296000],
-    constrainOnlyCenter: true
+    center: [2600000, 1200000],
+    zoom: 8
   })
 });
 
-// --- Chargement des points ---
-fetch("./police.geojson")
-  .then(r => r.json())
-  .then(geojson => {
-    const features = new ol.format.GeoJSON().readFeatures(geojson, {
-      dataProjection: "EPSG:2056",
-      featureProjection: "EPSG:2056"
-    });
-
-    pointLayer = new ol.layer.Vector({
-      source: new ol.source.Vector({ features }),
-      style: new ol.style.Style({
-        image: new ol.style.Circle({
-          radius: 6,
-          fill: new ol.style.Fill({ color: "red" })
-        })
-      })
-    });
-
-    map.addLayer(pointLayer);
-  });
-
-// --- Chargement de la Suisse + clipping ---
+// --- Masque vectoriel pour clipper la Suisse ---
 fetch("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson")
   .then(r => r.json())
   .then(data => {
@@ -74,59 +55,30 @@ fetch("https://raw.githubusercontent.com/datasets/geo-countries/master/data/coun
       featureProjection: "EPSG:2056"
     });
 
-    const switzerlandGeom = features[0].getGeometry();
-    const wmsLayer = map.getLayers().item(0);
+    const swissGeom = features[0].getGeometry();
 
-    // --- Clipping MultiPolygon + MultiRings ---
-    const applyClip = (event) => {
-      const ctx = event.context;
-      const pixelRatio = event.frameState.pixelRatio;
+    // --- Grand rectangle couvrant toute la Suisse ---
+    const worldExtent = [2420000, 1030000, 2900000, 1360000];
+    const worldPoly = ol.geom.Polygon.fromExtent(worldExtent);
 
-      ctx.save();
-      ctx.beginPath();
+    // --- Création du masque avec trou ---
+    const maskGeom = new ol.geom.Polygon(worldPoly.getCoordinates());
+    maskGeom.appendLinearRing(swissGeom.getLinearRing(0)); // trou = Suisse
 
-      const drawPolygon = (polygon) => {
-        const rings = polygon.getCoordinates();
-        rings.forEach(ring => {
-          ring.forEach((coord, i) => {
-            const pixel = map.getPixelFromCoordinate(coord);
-            if (i === 0) ctx.moveTo(pixel[0] * pixelRatio, pixel[1] * pixelRatio);
-            else ctx.lineTo(pixel[0] * pixelRatio, pixel[1] * pixelRatio);
-          });
-          ctx.closePath();
-        });
-      };
+    const maskFeature = new ol.Feature(maskGeom);
 
-      if (switzerlandGeom.getType() === "MultiPolygon") {
-        switzerlandGeom.getPolygons().forEach(drawPolygon);
-      } else {
-        drawPolygon(switzerlandGeom);
-      }
+    const maskLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({ features: [maskFeature] }),
+      style: new ol.style.Style({
+        fill: new ol.style.Fill({
+          color: "rgba(0,0,0,0.6)" // zone hors Suisse
+        })
+      })
+    });
 
-      ctx.clip();
-    };
+    map.addLayer(maskLayer);
 
-    const removeClip = (event) => {
-      event.context.restore();
-    };
-
-    // --- Clipping sur le WMS ---
-    wmsLayer.on("prerender", applyClip);
-    wmsLayer.on("postrender", removeClip);
-
-    // --- Clipping sur les points une fois chargés ---
-    const waitForPoints = () => {
-      if (pointLayer) {
-        pointLayer.on("prerender", applyClip);
-        pointLayer.on("postrender", removeClip);
-        map.render();
-      } else {
-        setTimeout(waitForPoints, 100);
-      }
-    };
-    waitForPoints();
-
-    // --- Contour de la Suisse ---
+    // --- Contour Suisse ---
     map.addLayer(
       new ol.layer.Vector({
         source: new ol.source.Vector({ features }),
