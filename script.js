042f35c9b5a047cb342f68c120fbedc1470ad731
Matthias Renaud <matthiasrenaud238@gmail.com>
2026-05-05 17:57:02 +0200

    0.1

--- script.js
+++ script.js
@@ -1,105 +1,110 @@
-// --- Projection LV95 ---
+const extent = [2420000, 1030000, 2900000, 1360000];
 proj4.defs(
   "EPSG:2056",
-  "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 " +
-  "+x_0=2600000 +y_0=1200000 +ellps=bessel " +
-  "+towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
+  "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000" +
+  " +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs"
 );
 ol.proj.proj4.register(proj4);
+const projection = new ol.proj.Projection({ code: "EPSG:2056", extent });
 
-const projection = new ol.proj.Projection({
-  code: "EPSG:2056",
-  extent: [2400000, 1000000, 3000000, 1500000]
-});
+let pointLayer;
 
-// --- Carte WMTS Swisstopo (URL correcte) ---
-const wmtsLayer = new ol.layer.Tile({
-  source: new ol.source.WMTS({
-    url: "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.jpeg",
-    layer: "ch.swisstopo.pixelkarte-farbe",
-    matrixSet: "2056",
-    format: "image/jpeg",
-    style: "default",
-    tileGrid: new ol.tilegrid.WMTS({
-      origin: [2420000, 1350000],
-      resolutions: [
-        4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000,
-        1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5
-      ],
-      matrixIds: Array.from({ length: 23 }, (_, i) => i.toString())
-    })
-  })
-});
-
-// --- Carte ---
 const map = new ol.Map({
   target: "map",
-  layers: [wmtsLayer],
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.TileWMS({
+        url: "https://wms.geo.admin.ch/de/",
+        params: {
+          LAYERS: "ch.swisstopo.pixelkarte-farbe",
+          FORMAT: "image/png",
+        },
+        serverType: "mapserver",
+      }),
+    }),
+  ],
   view: new ol.View({
     projection,
-    center: [2600000, 1200000],
-    zoom: 8
-  })
+    center: [2550000, 1207000],
+    zoom: 5,
+    extent: [2485000, 1075000, 2834000, 1296000],
+    constrainOnlyCenter: true,
+  }),
 });
 
-// --- Charger la Suisse ---
+fetch("./police.geojson")
+  .then(response => response.json())
+  .then(geojson => {
+    const features = new ol.format.GeoJSON().readFeatures(geojson, {
+      dataProjection: "EPSG:2056",
+      featureProjection: "EPSG:2056",
+    });
+    pointLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({ features }),
+      style: new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 6,
+          fill: new ol.style.Fill({ color: "red" }),
+        }),
+      }),
+    });
+    map.addLayer(pointLayer);
+  });
+
 fetch("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson")
   .then(r => r.json())
   .then(data => {
     const switzerland = data.features.find(f => f.properties.ISO_A2 === "CH");
-
     const features = new ol.format.GeoJSON().readFeatures(switzerland, {
       dataProjection: "EPSG:4326",
-      featureProjection: "EPSG:2056"
+      featureProjection: "EPSG:2056",
     });
+    const switzerlandGeom = features[0].getGeometry();
+    const wmsLayer = map.getLayers().item(0);
 
-    const swissGeom = features[0].getGeometry();
+    const applyClip = (event) => {
+      const ctx = event.context;
+      const pixelRatio = event.frameState.pixelRatio;
+      ctx.save();
+      ctx.beginPath();
 
-    // --- Couche contour Suisse ---
-    const borderLayer = new ol.layer.Vector({
+      const polygons = switzerlandGeom.getType() === "MultiPolygon"
+        ? switzerlandGeom.getPolygons()
+        : [switzerlandGeom];
+
+      polygons.forEach(polygon => {
+        polygon.getCoordinates()[0].forEach((coords, i) => {
+          const pixel = map.getPixelFromCoordinate(coords);
+          if (i === 0) ctx.moveTo(pixel[0] * pixelRatio, pixel[1] * pixelRatio);
+          else ctx.lineTo(pixel[0] * pixelRatio, pixel[1] * pixelRatio);
+        });
+        ctx.closePath();
+      });
+
+      ctx.clip();
+    };
+
+    const removeClip = (event) => event.context.restore();
+
+    wmsLayer.on("prerender", applyClip);
+    wmsLayer.on("postrender", removeClip);
+
+    const applyClipToPoints = () => {
+      if (pointLayer) {
+        pointLayer.on("prerender", applyClip);
+        pointLayer.on("postrender", removeClip);
+        map.render();
+      } else {
+        setTimeout(applyClipToPoints, 100);
+      }
+    };
+    applyClipToPoints();
+
+    map.addLayer(new ol.layer.Vector({
       source: new ol.source.Vector({ features }),
       style: new ol.style.Style({
-        stroke: new ol.style.Stroke({ color: "black", width: 2 }),
-        fill: null
-      })
-    });
-    map.addLayer(borderLayer);
-
-    // --- Couche masque (canvas renderer) ---
-    const maskLayer = new ol.layer.Vector({
-      source: new ol.source.Vector(),
-      style: new ol.style.Style({
-        renderer: (pixelCoords, state) => {
-          const ctx = state.context;
-          ctx.save();
-
-          // Remplir tout l'écran en blanc
-          ctx.fillStyle = "white";
-          ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-          // Découper la Suisse
-          ctx.globalCompositeOperation = "destination-out";
-
-          const geom = swissGeom.clone();
-          geom.transform("EPSG:2056", state.coordinateToPixelTransform);
-
-          const flat = geom.getFlatCoordinates();
-          const stride = geom.getStride();
-
-          ctx.beginPath();
-          for (let i = 0; i < flat.length; i += stride) {
-            const x = flat[i];
-            const y = flat[i + 1];
-            if (i === 0) ctx.moveTo(x, y);
-            else ctx.lineTo(x, y);
-          }
-          ctx.closePath();
-          ctx.fill();
-
-          ctx.restore();
-        }
-      })
-    });
-
-    map.addLayer(maskLayer);
+        stroke: new ol.style.Stroke({ color: "blue", width: 2 }),
+        fill: new ol.style.Fill({ color: "rgba(0,0,0,0)" }),
+      }),
+    }));
   });
