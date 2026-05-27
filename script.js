6c042d075084ee5a9797a0a97e8a4fb9f415a751
Matthias Renaud <matthiasrenaud238@gmail.com>
2026-05-27 13:23:57 +0200

    candidat=1

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function wgs84ToLv95(coord) { return ol.proj.transform(coord, "EPSG:4326", "EPSG
 
 // ── POI sources ──
 const poiSources = {};
-const CANDIDATES = 5;
+const CANDIDATES = 1;
 
 function getNearestCandidates(lv95Coord, color, n = CANDIDATES) {
   const source = poiSources[color];
